3bd28f387a13bc985eb0a83d983f17f58a0b0978
RichardZJM <richardzjm@gmail.com>
2023-01-02 20:10:41 -0500

    Undoing absolute position footer.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,17 +4,8 @@ import { useNavigate } from "react-router-dom";
 import "./Footer.css";
 function Footer() {
   const navigate = useNavigate();
-  const [fixFooter, setFixFooter] = useState(false);
-
-  useEffect(() => {
-    setFixFooter(document.documentElement.offsetHeight < window.innerHeight);
-  });
-
-  console.log(document.documentElement.offsetHeight);
-  console.log(window.innerHeight);
-  const sx = { left: 0, bottom: 0, right: 0, position: "absolute" };
   return (
-    <BottomNavigation className="footer-container" sx={fixFooter ? sx : {}}>
+    <BottomNavigation className="footer-container">
       <Button variant="outlined" size="small" onClick={() => navigate("/home")}>
         Home
       </Button>
