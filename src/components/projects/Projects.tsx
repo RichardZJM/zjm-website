b4ae60b0e0b917470ad0cb28e6be39a5103e9d8e
RichardZJM <richardzjm@gmail.com>
2023-02-16 18:42:46 -0500

    New bracnh updaing project and resume for jobs

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -10,26 +10,45 @@ import lttp from "../../images/projects/lttp.jpg";
 import structural from "../../images/projects/structural.jpg";
 import apdl from "../../images/projects/apdltools.png";
 import same from "../../images/projects/same.png";
+import mtp from "../../images/projects/mtp.jpg";
 
 function Projects() {
   return (
     <Container maxWidth="xl">
       <section className="projects-container">
         <ProjectsCard
+          title="Machine Learning Interatomic Potentials"
+          subtitle="Undergraduate Research Project: Training of a Potassium Metal Potential for Molecular Dynamics"
+          image={mtp}
+          chips={[
+            "Ongoing",
+            "Python",
+            "HPC Clusters",
+            "Active Machine Learning",
+            "Molecular Dynamics",
+            "DFT",
+          ]}
+          description={`Molecular dynamics simulations are depened on interatomic potentials to descibe the movement of atoms as classical particles. Quantum chemistry technqiues like Density Functional Theory provide an accurate but infeasibly expensive solution. Machine learning potentials can provide practical prediction models through training on quantum mechanical datasets. As part of the Queen's Nuclear Group, datasets are generated and processed on Compute Canada HPC clusters. Subsequent active and passive training of an MTP model is used to generated a potassium metal model which is validated against DFT results and physical property prediciton.`}
+          githubLink="https://github.com/RichardZJM/K-MTP-training"
+        />
+
+        <ProjectsCard
           title="Pin-Jointed Structure Simulator"
           subtitle="Simulation of User-Defined Structures Under Gravitational Load"
           image={structural}
           chips={[
+            "React",
             "TypeScript",
             "Energy Minimization",
             "Structural Analysis",
             "Graph Theory",
             "Numerical Optimization",
           ]}
-          description={`An simulation tool that calculates the stress and deformation of a pin-jointed structure under load. Users can use the interactive GUI to graphically plot free and fixed nodes. Links are defined through the selection of nodes. The structure's material and link profile design can also be specified. The solution is generated through potential energy minimization and stresses are plotted by colour. Try it out and simulate classic structures like towers, bridges, and cantilevers! `}
+          description={`An simulation tool that calculates the stress and deformation of a pin-jointed structure under load. Users can use the interactive GUI to graphically plot free and fixed nodes. Links are defined through the selection of nodes. The structure's material and link profile design can also be specified. The solution is generated through potential energy minimization and stresses are plotted by colour. Try it out and simulate classic structures like towers, bridges, and cantilevers! Prototyped in Python, built with React.`}
           githubLink="https://github.com/RichardZJM/zjm-website/tree/structural-solver/src/components/projects/structuralSolver"
           tryItOut="/projects/structural-solver"
         />
+
         <ProjectsCard
           title="SAM-e Fluid Modelling"
           subtitle="Capstone: Development of a GUI based CFD model with OpenFOAM Solvers for Condair Inc."
@@ -41,7 +60,9 @@ function Projects() {
             "Fluid Mechanics",
             "Model Tuning and Optimization",
           ]}
-          description={"The SAM-e is a steam distribution manifold manufactured by Condair Inc. As an undergraduate engineering capstone, a representative model is being created to help Condair experiment with model-driven design and develop the next generation of product. With a group of five students, an OpenFoam computational fluid dynamics model is being developed. Experimental data provided by Condair will be used to validate the model, on top which the team will create a user interfaces to increase usability for Condair engineering teams. "}
+          description={
+            "The SAM-e is a steam distribution manifold manufactured by Condair Inc. As an undergraduate engineering capstone, a representative model is being created to help Condair experiment with model-driven design and develop the next generation of product. With a group of five students, an OpenFoam computational fluid dynamics model is being developed. Experimental data provided by Condair will be used to validate the model, on top which the team will create a user interfaces to increase usability for Condair engineering teams. "
+          }
           explore={"https://youtu.be/Nsgq0Kj2ymU"}
         />
         <ProjectsCard
